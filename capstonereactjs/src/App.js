96da29defb63be07d969f505be2d2b0bd3f72fad
ZZBoxZZz <huydang1609.dpmh@gmail.com>
2023-03-01 08:17:17 +0700

    third update

--- capstonereactjs/src/App.js
+++ capstonereactjs/src/App.js
@@ -3,21 +3,26 @@ import { createBrowserHistory } from 'history';
 import { Router, Switch } from "react-router";
 import { HomeTemplate } from "./templates/HomeTemplate/HomeTemoplate";
 import Home from "./pages/Home/Home";
+import Contact from "./pages/Contact/Contact";
+import News from "./pages/News/News";
 export const history = createBrowserHistory();
 
 
 function App() {
   return (
-    <Router history ={history}>
-    
-      <Switch>
+    <Router history={history}>
 
-        <HomeTemplate path ='/' exact Component={Home}/>
+      <Switch>
+        <HomeTemplate path='/home' exact Component={Home} />
+        <HomeTemplate path='/contact' exact Component={Contact} />
+        <HomeTemplate path='/news' exact Component={News} />
+        <HomeTemplate path='/' exact Component={Home} />
+
       </Switch>
-    
-    
-    
-    
+
+
+
+
     </Router>
   );
 }
