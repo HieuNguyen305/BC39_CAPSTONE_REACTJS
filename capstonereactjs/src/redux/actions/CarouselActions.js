7a0e7a3cb05d2bf931df0fa7b2b038d32707fd8a
ZZBoxZZz <huydang1609.dpmh@gmail.com>
2023-03-04 01:44:41 +0700

    fourupdate

--- capstonereactjs/src/redux/actions/CarouselActions.js
+++ capstonereactjs/src/redux/actions/CarouselActions.js
@@ -1,8 +1,26 @@
+import axios from "axios";
+import { quanLyPhimService } from "../../services/quanLyPhimService";
+import { DOMAIN } from "../../util/settings/config";
+import { SET_CAROUSEL } from "../types/CarouselType";
+
+
+export const getCarouselAction = (thamSo) => {
+
+
+    return async (dispatch) => {
+
+        try {
+            const result = await quanLyPhimService.layDanhSachBanner();
+
+            dispatch({
+                tpye: SET_CAROUSEL,
+                arrImg: result.content.data
+            })
+        } catch (errors) {
+            console.log('errors', errors);
+        }
 
 
 
-export const getCarouselAction = () => {
-    return dispatch =>{
-        
-    }
+    };
 }
--- capstonereactjs/src/templates/HomeTemplate/Layout/HomeCarousel/HomeCarousel.js
+++ capstonereactjs/src/templates/HomeTemplate/Layout/HomeCarousel/HomeCarousel.js
@@ -1,9 +1,10 @@
-import React, {useEffect, useState} from 'react'
+import React, { useEffect } from 'react'
 import { Carousel } from 'antd';
-import { useSelector } from 'react-redux';
-import { repeat } from 'lodash';
+import { useSelector, useDispatch } from 'react-redux';
+
 
 import axios from 'axios';
+import { getCarouselAction } from '../../../../redux/actions/CarouselActions';
 
 const contentStyle = {
     height: '800px',
@@ -20,48 +21,36 @@ export default function HomeCarousel(props) {
 
     const { arrImg } = useSelector(state => state.CarouselReducer)
 
-
-    function useEffect(){
-        const[posts,setPosts] = useState([])
-
-        const[userId,serUserId] = useState("");
+    const dispatch = useDispatch();
 
 
-        const fetchData = () =>{
-            axios
-                .get("https://movienew.cybersoft.edu.vn/api/QuanLyPhim/LayDanhSachBanner",{
-                    params: {
-                        userId: userId || undefined,
-                    },
-                })
-                .then((respone)=>{
-                    setPosts(respone.data);
-                })
-                .catch((error)=>{
-                    console.log(error);
-                });
-        };
+    useEffect(() => {
+
+        //const action = getCarouselAction(1);
+
+        dispatch(getCarouselAction());
+
+    }, [])
 
 
-    }
+
 
     const renderImg = () => {
 
         return arrImg.map((item, index) => {
             return <div key={index}>
 
-                <div style={{...contentStyle, background: `url(${item.hinhAnh})`}}>
-                    
-                        <img src={item.hinhAnh} className='w-full h-full' alt={item.hinhAnh} />
-                    
+                <div style={{ ...contentStyle, background: `url(${item.hinhAnh})` }}>
+
+                    <img src={item.hinhAnh} className='w-full h-full' alt={item.hinhAnh} />
+
                 </div>
             </div>
         })
     }
 
     return (
-        <Carousel autoplay style={{ position: 'relative' }}>
-
+        <Carousel effect='fade'>
             {renderImg()}
         </Carousel>
     )
